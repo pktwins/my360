0f7a35718336255cf9af12fa36576360fae0ae7d
Ramakrishna <m.ramkrishna567@gmail.com>
2022-07-16 11:25:05 +0530

    changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class my360 extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            Welcome to Virtual Reality
           </Text>
         </View>
       </View>
